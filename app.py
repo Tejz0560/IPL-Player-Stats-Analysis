799290a4269944aad9382cc92670731bd2961cd9
Tejz0560 <tejbidwai@gmail.com>
2022-12-21 15:22:35 +0530

    team vs team api added

--- app.py
+++ app.py
@@ -6,15 +6,24 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     response = {
-        
+        "A-message" : "Welcome to the IPL api service by Tejas Bidwai. Here are the apis to help you : ",
         "api/allteams":" List of all Teams",
-        "A-message" : "Welcome to the IPL api service by Tejas Bidwai. Here are the apis to help you : "
+        "api/teamvteam":"It gives team1 vs team2 stats. Input required team1 = first team, team2 = second team. eg,: api/teamvteam?team1=Mumbai Indians&team2=Chennai Super Kings"
     }
     return jsonify(response)
 
 @app.route("/api/allteams")
 def teams():
     response = ipl.teamsAPI()
+    return jsonify(response)
+
+@app.route("/api/teamvteam")
+def teamvteam():
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+
+    response = ipl.teamVteamAPI(team1,team2)
+
     return jsonify(response)
 
 
