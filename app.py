eeff20952e75756a0cfd1f4e4b434c018d0c2a8b
Tejz0560 <tejbidwai@gmail.com>
2022-12-28 10:48:46 +0530

    minor changes

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ def home():
     response = {
         "A-message" : "Welcome to the IPL api service by Tejas Bidwai. Here are the apis to help you : ",
         "api/allteams":" List of all Teams",
-        "api/teamvteam":"It gives team1 vs team2 stats. Input required team1 = first team, team2 = second team. eg,: api/teamvteam?team1=Mumbai Indians&team2=Chennai Super Kings"
+        "api/teamvteam":"It gives team1 vs team2 stats. Input required team1 = first team, team2 = second team. eg,: api/teamvteam?team1=Mumbai Indians&team2=Chennai Super Kings",
+        "api/teamrecord":"It gives team stats vs all other teams. Input required team = name of team. eg.: api/teamrecord?team=Mumbai Indians",
+        "api/batsmanrecord":"It gives batsman stats vs all other teams. Input required batsman = name of batsman. eg.: api/batsmanrecord?batsman=V Kohli",
+        "api/bowlerrecord":"It gives bowler stats vs all other teams. Input required bowler = name of bowler. eg.: api/bowlerrecord?bowler=JJ Bumrah"
+        
+
     }
     return jsonify(response)
 
